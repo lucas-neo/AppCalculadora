ad6205fda6f9a92caa85024b6c820ba05b71e1a2
Lucas Neo <lucasneo@outlook.com.br>
2026-03-03 21:17:07 -0300

    Alter file App.js

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View >
+        <Text style={styles.display}></Text>
+      </View>
+      <View>
+        <View style={styles.row}>
+        <TouchableOpacity>7</TouchableOpacity>
+        <TouchableOpacity>8</TouchableOpacity>
+        <TouchableOpacity>9</TouchableOpacity>
+        <TouchableOpacity>/</TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+        <TouchableOpacity>4</TouchableOpacity>
+        <TouchableOpacity>5</TouchableOpacity>
+        <TouchableOpacity>6</TouchableOpacity>
+        <TouchableOpacity>*</TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+        <TouchableOpacity>1</TouchableOpacity>
+        <TouchableOpacity>2</TouchableOpacity>
+        <TouchableOpacity>3</TouchableOpacity>
+        <TouchableOpacity>-</TouchableOpacity>
+        </View>
+        <View style={styles.row}>
+        <TouchableOpacity>0</TouchableOpacity>
+        <TouchableOpacity>√</TouchableOpacity>
+        <TouchableOpacity>=</TouchableOpacity>
+        <TouchableOpacity>+</TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
@@ -17,4 +45,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  row: {
+    flexDirection: 'row'
+  },
+  display: {
+    borderColor: 'black'
+  }
 });
