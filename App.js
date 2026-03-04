3a07b7af794d471cecc0d710a1dc596e2ad9e039
Lucas Neo <lucasneo@outlook.com.br>
2026-03-03 22:08:11 -0300

    Feat: Add number and display components

--- App.js
+++ App.js
@@ -1,37 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 
 export default function App() {
+
+  const [display, setDisplay] = useState('0')
+
+  function updateDisplay(num){
+    return setDisplay( )
+  }
+
   return (
     <View style={styles.container}>
-      <View >
-        <Text style={styles.display}></Text>
+      <View style={styles.display} >
+        <Text style={styles.digito}>{display}</Text>
       </View>
       <View>
-        <View style={styles.row}>
-        <TouchableOpacity>7</TouchableOpacity>
-        <TouchableOpacity>8</TouchableOpacity>
-        <TouchableOpacity>9</TouchableOpacity>
-        <TouchableOpacity>/</TouchableOpacity>
+        <View style={styles.linha}>
+          <TouchableOpacity onPress={()=> updateDisplay(7)}>
+            <Text style={styles.row}>7</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=> updateDisplay(8)}>
+            <Text style={styles.row}>8</Text>
+          </TouchableOpacity >
+          <TouchableOpacity onPress={()=> updateDisplay(9)}>
+            <Text style={styles.row}>9</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.row}>/</Text>
+          </TouchableOpacity>
         </View>
-        <View style={styles.row}>
-        <TouchableOpacity>4</TouchableOpacity>
-        <TouchableOpacity>5</TouchableOpacity>
-        <TouchableOpacity>6</TouchableOpacity>
-        <TouchableOpacity>*</TouchableOpacity>
+        <View style={styles.linha}>
+          <TouchableOpacity onPress={()=> updateDisplay(4)}>
+            <Text style={styles.row}>4</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=> updateDisplay(5)}>
+            <Text style={styles.row}>5</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=> updateDisplay(6)}>
+            <Text style={styles.row}>6</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.row}>*</Text>
+          </TouchableOpacity>
         </View>
-        <View style={styles.row}>
-        <TouchableOpacity>1</TouchableOpacity>
-        <TouchableOpacity>2</TouchableOpacity>
-        <TouchableOpacity>3</TouchableOpacity>
-        <TouchableOpacity>-</TouchableOpacity>
+        <View style={styles.linha}>
+          <TouchableOpacity onPress={()=> updateDisplay(1)}>
+            <Text style={styles.row}>1</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=> updateDisplay(2)}>
+            <Text style={styles.row}>2</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={()=> updateDisplay(3)}>
+            <Text style={styles.row}>3</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.row}>-</Text>
+          </TouchableOpacity>
         </View>
-        <View style={styles.row}>
-        <TouchableOpacity>0</TouchableOpacity>
-        <TouchableOpacity>√</TouchableOpacity>
-        <TouchableOpacity>=</TouchableOpacity>
-        <TouchableOpacity>+</TouchableOpacity>
+        <View style={styles.linha}>
+          <TouchableOpacity onPress={()=> updateDisplay(0)}>
+            <Text style={styles.row}>0</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.row}>√</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.row}>=</Text>
+          </TouchableOpacity>
+          <TouchableOpacity>
+            <Text style={styles.row}>+</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -44,11 +84,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    
+  },
+  linha: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    padding: 2
   },
   row: {
-    flexDirection: 'row'
+    fontSize: 40,
+    padding: 20,
+    paddingHorizontal: 27,
   },
   display: {
-    borderColor: 'black'
+    backgroundColor: '#000000ff',
+    width: 300,
+    padding: 30,
+  },
+  teclado: {
+    marginTop: 10
+  },
+  digito: {
+    color: '#FFFFFF'
   }
 });
